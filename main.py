4be0183a66131138fe673cfdb6499f3fdd17f218
Gabriel-Caceres <gabr.caceres@duocuc.cl>
2025-06-14 12:30:13 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+opcion = 0
+
+est_civil = {"C":"Casado",
+             "S":"Soltero",
+             "V":"Viudo"}
+
+generos = {"M":"Masculino",
+           "F":"Femenino",}
+
+dicc = {"Rut": ["219766742"],
+        "Nombre": ["Gabriel Caceres"],
+        "Edad": ["19"],
+        "Estado Civil": [est_civil["S"]],
+        "Genero": [generos["M"]],
+        "fecha": "13-06-2025"}
+
+print(dicc)
